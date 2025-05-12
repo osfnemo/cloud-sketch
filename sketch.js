f6aa27f114191db521581df1fe6356c76815ab87
osfnemo <nema.adel@gmail.com>
2025-05-12 14:58:42 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,9 +3,11 @@ let img;
 let tiles = [];
 let cols, rows;
 let tileSize = 24;
+let lastMoved = 0;
+let fadeEdge; // for vignette
 
 function preload() {
-  img = loadImage("cloud.png?v=5");
+  img = loadImage("cloud.png?v=8");
 }
 
 function setup() {
@@ -13,11 +15,29 @@ function setup() {
   noSmooth();
   createCanvas(windowWidth, windowHeight);
   initTiles();
+  fadeEdge = createGraphics(width, height);
+  fadeEdge.noFill();
+  for (let i = 0; i < 100; i++) {
+    fadeEdge.stroke(0, 0, 0, map(i, 0, 100, 0, 255));
+    fadeEdge.strokeWeight(2);
+    fadeEdge.rect(i, i, width - i * 2, height - i * 2);
+  }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   initTiles();
+  fadeEdge = createGraphics(width, height);
+  fadeEdge.noFill();
+  for (let i = 0; i < 100; i++) {
+    fadeEdge.stroke(0, 0, 0, map(i, 0, 100, 0, 255));
+    fadeEdge.strokeWeight(2);
+    fadeEdge.rect(i, i, width - i * 2, height - i * 2);
+  }
+}
+
+function mouseMoved() {
+  lastMoved = millis();
 }
 
 function initTiles() {
@@ -31,18 +51,7 @@ function initTiles() {
 
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
-      let tile = createImage(tileSize, tileSize);
-      tile.copy(
-        img,
-        x * tileSize,
-        y * tileSize,
-        tileSize,
-        tileSize,
-        0,
-        0,
-        tileSize,
-        tileSize
-      );
+      let tile = img.get(x * tileSize, y * tileSize, tileSize, tileSize);
       tiles.push({
         img: tile,
         x: x * tileSize + offsetX,
@@ -59,20 +68,24 @@ function initTiles() {
 function draw() {
   clear();
 
+  let mx = constrain(mouseX, 0, width);
+  let my = constrain(mouseY, 0, height);
+  let fade = constrain(map(millis() - lastMoved, 0, 1200, 1, 0), 0, 1);
+
   for (let t of tiles) {
-    let dx = t.x + tileSize / 2 - mouseX;
-    let dy = t.y + tileSize / 2 - mouseY;
+    let dx = t.x + tileSize / 2 - mx;
+    let dy = t.y + tileSize / 2 - my;
     let d = sqrt(dx * dx + dy * dy);
 
     if (d < 150) {
       let angle = atan2(dy, dx);
-      let force = map(d, 0, 150, 8, 0);
+      let force = map(d, 0, 150, 10, 0) * fade;
       t.vx += cos(angle) * force;
       t.vy += sin(angle) * force;
     }
 
-    let ax = (t.ox - t.x) * 0.1;
-    let ay = (t.oy - t.y) * 0.1;
+    let ax = (t.ox - t.x) * 0.08;
+    let ay = (t.oy - t.y) * 0.08;
 
     t.vx += ax;
     t.vy += ay;
@@ -83,6 +96,8 @@ function draw() {
     t.x += t.vx;
     t.y += t.vy;
 
-    image(t.img, int(t.x), int(t.y));
+    image(t.img, t.x, t.y);
   }
+
+  image(fadeEdge, 0, 0); // Apply soft edge vignette
 }
